066c5574ad3df1e95edeab267b43bdd4bfc57548
laiyurong <shinnosuke0875@gmail.com>
2017-05-01 23:28:09 +0800

    Initial Commit

---
+++ baseballRecord/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  baseballRecord
+//
+//  Created by 賴昱榮 on 2017/5/1.
+//  Copyright © 2017年 賴昱榮. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
