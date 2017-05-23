e867c8f1b04e4aab6a843df43ef76a8f92322bb9
laiyurong <shinnosuke0875@gmail.com>
2017-05-23 13:30:41 +0800

    5/23

--- baseballRecord/ViewController.swift
+++ baseballRecord/ViewController.swift
@@ -8,10 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController , UIPickerViewDelegate , UIPickerViewDataSource {
+    @IBOutlet var oneTapForSrike: UITapGestureRecognizer!
+
+    @IBAction func hit(_ sender: Any) {
+        
+    }
+    @IBOutlet weak var hitResult: UIView!
+    @IBOutlet weak var picker: UIPickerView!
+    var pickerData: [String] = [String]()
+
+    @IBAction func buttonClicked(sender: AnyObject) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        pickerData = ["FO","AO","1B","2B","3B","HR","FC","SF","SH","DP","TP"]
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +33,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(_pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
 
 }
 
