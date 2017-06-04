a56514fc49f198a68c2ef1480b4b519d7e5f7e60
sean <sean@MACde-MacBook-Air.local>
2017-06-05 00:48:12 +0800

    done of player info

--- baseballRecord/ViewController.swift
+++ baseballRecord/ViewController.swift
@@ -8,8 +8,51 @@
 
 import UIKit
 
+class Player{
+    var name = ""
+    var atBat = 0 //打擊次數
+    var hit = 0 //安打數
+    init(as name: String) {
+        self.name = name
+    }
+    func setName(as name: String){
+        self.name = name
+    
+    }
+    func getName() -> String {
+        return self.name
+    }
+    func addHit(){
+        self.hit += 1
+        self.atBat += 1
+    }
+    func addOut(){
+        
+        self.atBat += 1
+    }
+    func getHit() ->String{
+        return String(self.hit)
+    }
+    func getAtBat() -> String{
+        return String(self.atBat)
+    }
+    func getBA() -> String{
+        var BA:Float
+        if self.atBat == 0{
+            BA = 0.000
+        }else{
+            BA = Float(self.hit / self.atBat)
+        }
+        return String(format:"%.3f", BA )
+    }
+}
+
 
 class ViewController: UIViewController{
+    var players = [String: Player]()
+    var playerClassList = [Player]()
+    
+    
     @IBOutlet var awayPlayer9: UILabel!
     @IBOutlet var awayPlayer8: UILabel!
     @IBOutlet var awayPlayer7: UILabel!
@@ -29,7 +72,7 @@ class ViewController: UIViewController{
     @IBOutlet var homePlayer3: UILabel!
     @IBOutlet var homePlayer2: UILabel!
     @IBOutlet var homePlayer1: UILabel!
-
+/*
     @IBOutlet var top1: UILabel!
     @IBOutlet var bottom1: UILabel!
     @IBOutlet var top2: UILabel!
@@ -48,14 +91,20 @@ class ViewController: UIViewController{
     @IBOutlet var bottom8: UILabel!
     @IBOutlet var top9: UILabel!
     @IBOutlet var bottom9: UILabel!
-
-
+*/
+    //print("player")
     @IBOutlet var outCount: UILabel!
     @IBOutlet var innings: UILabel!
     @IBOutlet var awayScore: UILabel!
     @IBOutlet var homeScore: UILabel!
     @IBOutlet var awayHit: UILabel!
     @IBOutlet var homeHit: UILabel!
+    @IBOutlet var playerHit: UILabel! //上場打者打擊紀錄
+    @IBOutlet var playerName: UILabel! //上場打者名字
+    @IBOutlet var playerBA: UILabel! //上場打者打擊率
+
+
+//    @IBOutlet var dynamicPlayer: Player!
     var batters = [[UILabel]]()
     var inningScore = [[UILabel]]()
     var batterOn = [-1 , -1]
@@ -155,8 +204,10 @@ class ViewController: UIViewController{
             homeScoring = homeScoring + 1
             homeScore.text = String (homeScoring)
         }
-        thisInningScore = thisInningScore + 1
-        inningScore[awayOrHome][inning].text = String (thisInningScore)
+    }
+    
+    func setPlayerName (label: UILabel, name: String  ){
+        label.text? = "●\n\(name)"
     }
     
     func runner(batter: Int, bases: Int){
@@ -193,7 +244,12 @@ class ViewController: UIViewController{
                 })
                 UIView.animate(withDuration: 1.0,delay: TimeInterval(j) ,animations: {
                     self.batters[self.awayOrHome][i].alpha = 0.0
-                    self.batters[self.awayOrHome][i].center.x = 100
+                    if self.awayOrHome == 0{
+                        self.batters[self.awayOrHome][i].center.x = 100
+                    }
+                    if self.awayOrHome == 1{
+                        self.batters[self.awayOrHome][i].center.x = 360
+                    }
                     self.batters[self.awayOrHome][i].center.y = 250
                 })
                 scoring(whichTeam: awayOrHome)
@@ -223,23 +279,33 @@ class ViewController: UIViewController{
             }
         }
             out = 0
-            thisInningScore = 0
             inningCheck(whichTeam: awayOrHome)
         }
     }
     @IBAction func callNextBatter(sender: AnyObject){
+        self.batterOn[self.awayOrHome] = self.batterOn[self.awayOrHome] + 1
+        if self.batterOn[self.awayOrHome] == 9{
+            self.batterOn[self.awayOrHome] = 0
+        }
         UIView.animate(withDuration: 1.0,animations: {
-            self.batterOn[self.awayOrHome] = self.batterOn[self.awayOrHome] + 1
-            if self.batterOn[self.awayOrHome] == 9{
-                self.batterOn[self.awayOrHome] = 0
-            }
             self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].alpha = 1.0
             self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].center.x = 226
             self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].center.y = 260
          })
+//        打者資訊
+        playerName.text? = (players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.getName())!
+        playerBA.text? = (players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.getBA())!
+        playerHit.text? = "\((players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.getAtBat())!)-\((players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.getHit())!)"
     }
 
     @IBAction func batterOut(sender: AnyObject){
+        
+        players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.addOut()
+        //        打者打擊率
+        playerBA.text? = (players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.getBA())!
+        //        打者打擊紀錄
+        playerHit.text? = "\((players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.getAtBat())!)-\((players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.getHit())!)"
+        
         UIView.animate(withDuration: 1.0,animations: {
             self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].alpha = 0.0
             if self.awayOrHome == 0 {
@@ -256,18 +322,45 @@ class ViewController: UIViewController{
 
     @IBAction func takeOneBase(sender: AnyObject) {
         //0to1
+        
+        players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.addHit()
+        //        打者打擊率
+        playerBA.text? = (players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.getBA())!
+        //        打者打擊紀錄
+        playerHit.text? = "\((players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.getAtBat())!)-\((players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.getHit())!)"
+        
         self.runner(batter: self.batterOn[self.awayOrHome], bases: 1)
         self.hitCheck(whichTeam: self.awayOrHome)
+        
     }
     @IBAction func takeTwoBase( sender: AnyObject) {
+        
+        players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.addHit()
+        //        打者打擊率
+        playerBA.text? = (players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.getBA())!
+        //        打者打擊紀錄
+        playerHit.text? = "\((players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.getAtBat())!)-\((players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.getHit())!)"
+        
         self.runner(batter: self.batterOn[self.awayOrHome], bases: 2)
         self.hitCheck(whichTeam: self.awayOrHome)
     }
     @IBAction func takeThreeBase( sender: AnyObject) {
+        players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.addHit()
+        //        打者打擊率
+        playerBA.text? = (players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.getBA())!
+        //        打者打擊紀錄
+        playerHit.text? = "\((players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.getAtBat())!)-\((players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.getHit())!)"
+        
         self.runner(batter: self.batterOn[self.awayOrHome], bases: 3)
         self.hitCheck(whichTeam: self.awayOrHome)
     }
     @IBAction func takeFourBase( sender: AnyObject) {
+        players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.addHit()
+//        打者打擊率
+        playerBA.text? = (players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.getBA())!
+//        打者打擊紀錄
+        playerHit.text? = "\((players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.getAtBat())!)-\((players[self.batters[self.awayOrHome][self.batterOn[self.awayOrHome]].text!]?.getHit())!)"
+        
         self.runner(batter: self.batterOn[self.awayOrHome], bases: 4)
         self.hitCheck(whichTeam: self.awayOrHome)
     }
@@ -284,10 +377,90 @@ class ViewController: UIViewController{
     @IBAction func buttonClicked(sender: AnyObject) {
     }
  */
+//    func updateName(){
+//        homePlayer8.addhit()
+//    }
     override func viewDidLoad() {
         super.viewDidLoad()
+//        updateName()
         batters = [[awayPlayer1,awayPlayer2,awayPlayer3,awayPlayer4,awayPlayer5,awayPlayer6,awayPlayer7,awayPlayer8,awayPlayer9] , [homePlayer1,homePlayer2,homePlayer3,homePlayer4,homePlayer5,homePlayer6,homePlayer7,homePlayer8,homePlayer9]]
+        awayPlayer1.text? = "Kevin"
+        let kevin = Player(as: "Kevin")
+        players.updateValue(kevin, forKey: "Kevin")
+        
+        awayPlayer2.text? = "Robber"
+        let robber = Player(as: "Robber")
+        players.updateValue(robber, forKey: "Robber")
+        
+        awayPlayer3.text? = "Green"
+        let green = Player(as: "Green")
+        players.updateValue(green, forKey: "Green")
+        
+        awayPlayer4.text? = "Anderson"
+        let anderson = Player(as: "Anderson")
+        players.updateValue(anderson, forKey: "Anderson")
+        
+        awayPlayer5.text? = "Bob"
+        let bob = Player(as: "Bob")
+        players.updateValue(bob, forKey: "Bob")
+        
+        awayPlayer6.text? = "Gray"
+        let gray = Player(as: "Gray")
+        players.updateValue(gray, forKey: "Gray")
+        
+        awayPlayer7.text? = "John"
+        let john = Player(as: "John")
+        players.updateValue(john, forKey: "John")
+        
+        awayPlayer8.text? = "Wright"
+        let wright = Player(as: "Wright")
+        players.updateValue(wright, forKey: "Wright")
+        
+        awayPlayer9.text? = "Denny"
+        let denny = Player(as: "Denny")
+        players.updateValue(denny, forKey: "Denny")
+        
+        homePlayer1.text? = "Ann"
+        let ann = Player(as: "Ann")
+        players.updateValue(ann, forKey: "Ann")
+
+        homePlayer2.text? = "Young"
+        let young = Player(as: "Young")
+        players.updateValue(young, forKey: "Young")
+        
+        homePlayer3.text? = "Money"
+        let money = Player(as: "Money")
+        players.updateValue(money, forKey: "Money")
+        
+        homePlayer4.text? = "Fry"
+        let fry = Player(as: "Fry")
+        players.updateValue(fry, forKey: "Fry")
+        
+        homePlayer5.text? = "Carter"
+        let carter = Player(as: "Carter")
+        players.updateValue(carter, forKey: "Carter")
+        
+        homePlayer6.text? = "Penny"
+        let penny = Player(as: "Penny")
+        players.updateValue(penny, forKey: "Penny")
+        
+        homePlayer7.text? = "Len"
+        let len = Player(as: "Len")
+        players.updateValue(len, forKey: "Len")
+        
+        homePlayer8.text? = "Rose"
+        let rose = Player(as: "Rose")
+        players.updateValue(rose, forKey: "Rose")
+        
+        homePlayer9.text? = "Ted"
+        let ted = Player(as: "Ted")
+        players.updateValue(ted, forKey: "Ted")
+        
+        
+       
+        /*
         inningScore = [[top1,top2,top3,top4,top5,top6,top7,top8,top9],[bottom1,bottom2,bottom3,bottom4,bottom5,bottom6,bottom7,bottom8,bottom9]]
+ */
         /*
         super.viewDidLoad()
         self.picker.delegate = self
---
+++ baseballRecord/player.swift
@@ -0,0 +1,16 @@
+//
+//  player.swift
+//  baseballRecord
+//
+//  Created by sean on 2017/5/26.
+//  Copyright © 2017年 賴昱榮. All rights reserved.
+//
+
+import UIKit
+
+class player: UIViewController{
+    var name = "kevin"
+    @IBOutlet var playerA: UILabel!
+    
+    
+}
